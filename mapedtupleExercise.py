af084ca596dec9bb1f3125ee60dc7a4d469e1fc3
Sumit Ojha <sumit.ojha.dev@gmail.com>
2025-05-29 08:48:16 -0500

    Using mapedtuple and dataframe code intro

---
+++ mapedtupleExercise.py
@@ -0,0 +1,99 @@
+import os
+import urllib.request
+import ssl
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+urls_and_paths = {
+    "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/master/test.txt": os.path.join(data_dir, "test.txt"),
+    "https://github.com/saiadityaus1/SparkCore1/raw/master/winutils.exe": os.path.join(data_dir1, "winutils.exe"),
+    "https://github.com/saiadityaus1/SparkCore1/raw/master/hadoop.dll": os.path.join(data_dir1, "hadoop.dll")
+}
+
+# Create an unverified SSL context
+ssl_context = ssl._create_unverified_context()
+
+for url, path in urls_and_paths.items():
+    # Use the unverified context with urlopen
+    with urllib.request.urlopen(url, context=ssl_context) as response, open(path, 'wb') as out_file:
+        data = response.read()
+        out_file.write(data)
+import os, urllib.request, ssl; ssl_context = ssl._create_unverified_context(); [open(path, 'wb').write(urllib.request.urlopen(url, context=ssl_context).read()) for url, path in { "https://github.com/saiadityaus1/test1/raw/main/df.csv": "df.csv", "https://github.com/saiadityaus1/test1/raw/main/df1.csv": "df1.csv", "https://github.com/saiadityaus1/test1/raw/main/dt.txt": "dt.txt", "https://github.com/saiadityaus1/test1/raw/main/file1.txt": "file1.txt", "https://github.com/saiadityaus1/test1/raw/main/file2.txt": "file2.txt", "https://github.com/saiadityaus1/test1/raw/main/file3.txt": "file3.txt", "https://github.com/saiadityaus1/test1/raw/main/file4.json": "file4.json", "https://github.com/saiadityaus1/test1/raw/main/file5.parquet": "file5.parquet", "https://github.com/saiadityaus1/test1/raw/main/file6": "file6", "https://github.com/saiadityaus1/test1/raw/main/prod.csv": "prod.csv", "https://raw.githubusercontent.com/saiadityaus1/test1/refs/heads/main/state.txt": "state.txt", "https://github.com/saiadityaus1/test1/raw/main/usdata.csv": "usdata.csv", "https://github.com/saiadityaus1/SparkCore1/raw/refs/heads/master/data.orc": "data.orc", "https://github.com/saiadityaus1/test1/raw/main/usdata.csv": "usdata.csv", "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/refs/heads/master/rm.json": "rm.json"}.items()]
+
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] ="hadoop"
+os.environ['JAVA_HOME'] = r'C:\Program Files\Amazon Corretto\jdk1.8.0_452'
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+spark.read.format("csv").load("data/test.txt").toDF("Success").show(20, False)
+
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+data = sc.textFile("dt.txt")
+print()
+print("================RAW DATA===================")
+print()
+data.foreach(print)
+
+fildata = data.filter(lambda x: 'Gymnastics' in x)
+print()
+print("================FILDATA===================")
+print()
+fildata.foreach(print)
+
+# New requirement
+# Only show me the rows that have Gymnastics in fifth columns only
+
+mapsplit = data.map(lambda x: x.split(","))
+print()
+print("================MAPSPLIT===================")
+print()
+mapsplit.foreach(print)
+
+from collections import namedtuple
+
+columns = namedtuple('columns', ['id','tdate','amt','category','product','mode'])
+
+schemardd = mapsplit.map(lambda x: columns(x[0],x[1],x[2],x[3],x[4],x[5]))
+colfilter =schemardd.filter(lambda x:'Gymnastics' in x.product)
+print()
+print("================COLFILTER===================")
+print()
+colfilter.foreach(print)
+print()
+
+# After the column filter we need to write the data as parquet file
+
+# convert to dataframe first
+df=colfilter.toDF()
+df.show()
+
+# then save as parquet
+# df.write.parquet("file:///C:/Users\sumit\Documents/parquetData")
+
+
+
+
